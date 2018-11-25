c69c1b2ec45f82fc2ec5c92488310774188c9f14
GruzinskiyAl <gruzinskiy.al@gmail.com>
2018-11-25 02:30:51 +0200

    client and server basic tasks done, cant send request async

--- main.js
+++ main.js
@@ -1,29 +1,72 @@
-const dataContainer = document.querySelector("#allDataBlock");
+const fullDataContainer = document.querySelector("#fullData");
+const securitySettignsContainer = document.querySelector("#securitySettings");
+const tradingSessionsContainer = document.querySelector("#tradingSessions");
+
 const requestBtn = document.querySelector("#requestBtn");
-const req = new XMLHttpRequest();
+let req = new XMLHttpRequest();
+req.responseType = "json";
+
 const serverUrl = 'http://localhost:8099';
 
-requestBtn.addEventListener("click", getAllData)
+requestBtn.addEventListener("click", getData)
 
-function createDataBlock(data) {
-    const div = document.createElement();
+function renderData(data, wrapperEl) {
+    const div = document.createElement("DIV");
     div.innerText = data;
-    dataContainer.appendChild(div);
+    wrapperEl.appendChild(div);
 }
 
-function getAllData() {
-    req.responseType = 'json';
-    req.open('GET', serverUrl + "/");
-    req.onreadystatechange  = () => {
+function getFullData() {
+    req.open("GET", serverUrl + "/");
+
+    req.onreadystatechange = () => {
         if (req.readyState === 4) {
-            let jsonResponse = req.response;
-            let data = JSON.parse(jsonResponse);
-            createDataBlock(data);
+            if (req.status === 200) {
+                renderData(JSON.stringify(req.response),  fullDataContainer)
+            } else {
+                renderData("Cant read Data", fullDataContainer)
+            }
         }
-    };
-    req.send(null);
+        
+    }
+    req.send();
+};
+
+function getSecSettings() {
+    req.open("GET", serverUrl + "/security_settings");
+    req.send();
+
+    req.onreadystatechange = () => {
+        if (req.readyState === 4) {
+            console.log(req.status)
+            if (req.status === 200) {
+                renderData(JSON.stringify(req.response),  securitySettignsContainer)
+            } else {
+                renderData("Cant read Data", securitySettignsContainer)
+            }
+        }
+    }
+
 }
 
-// setInterval( () => {
-//     settingsRequest();
-// }, 5000);
+function getTradSessions() {
+    req.open("GET", serverUrl + "/trading_sessions");
+    req.send();
+
+    req.onreadystatechange = () => {
+        if (req.readyState === 4) {
+            if (req.status === 200) {
+                renderData(JSON.stringify(req.response), tradingSessionsContainer)
+            } else {
+                renderData("Cant read Data", tradingSessionsContainer)
+            }
+        }
+    }
+}
+
+function getData() {
+    return new Promise((res, rej) => {
+        getFullData();
+    })
+
+}
